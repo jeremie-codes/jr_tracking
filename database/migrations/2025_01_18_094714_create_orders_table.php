20d988441a91e1e4fc59bbef6edf38e81c38d0bc
Sal Aime <salemnk02@gmail.com>
2025-01-18 12:18:46 +0100

    update foreign

--- database/migrations/2025_01_18_094714_create_orders_table.php
+++ database/migrations/2025_01_18_094714_create_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('customer_id')->nullable()->constrained()->nullOnDelete();
+            // $table->foreignId('customer_id')->nullable()->constrained()->nullOnDelete();
             $table->string('number', 32)->unique();
             $table->decimal('total_price', 12, 2)->nullable();
             $table->enum('status', ['new', 'payer', 'shipped', 'delivered', 'cancelled'])->default('new');
